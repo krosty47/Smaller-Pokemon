993a8828bc7354022ef10d5031409ae2b1f0e25a
krosty47 <c_coriasco@hotmail.com>
2022-01-02 18:42:16 -0300

    Main styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-2x1 text-red-200">Hello World</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center align-center items-center">
+      <div className="text-2x1 text-center">Which Pokemón is Smaller?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex items-center justify-between max-w-2x1">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
