293b1f550291ee99cc246cd7720ea32c413ddd0a
krosty47 <c_coriasco@hotmail.com>
2022-01-19 15:35:16 -0300

    adding pscale db

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,12 @@
-import { appRouter } from "../../../backend/router/index";
+import { appRouter, AppRouter } from "../../../backend/router/index";
+import { inferProcedureOutput } from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => null,
 });
+
+export type inferQueryResponse<
+  TRouteKey extends keyof AppRouter["_def"]["queries"]
+> = inferProcedureOutput<AppRouter["_def"]["queries"][TRouteKey]>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,63 @@
-import { useState } from "react";
+import { useState, FC } from "react";
 import type { NextPage } from "next";
 import { trpc } from "@/utils/trpc";
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import { inferQueryResponse } from "./api/trpc/[trpc]";
 
-const Home: NextPage = () => {
+const btn =
+  "bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-1 border border-gray-400 rounded shadow";
+
+export default () => {
   const [ids, setIds] = useState(() => getOptionsForVote());
   const [first, second] = ids;
   const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
   const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
 
-  // console.log("firstPokemon", firstPokemon);
-  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+  const voteForRoundest = (selected: number) => {
+    setIds(getOptionsForVote());
+  };
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-center items-center">
       <div className="text-2x1 text-center">Which Pokemón is Smaller?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex items-center justify-between max-w-2x1">
-        <div className="w-64 h-64 flex flex-col">
-          <img
-            src={firstPokemon.data?.sprites.front_default!}
-            className="w-full"
-          />
-          <div className="text-xl text-center capitalize mt-[-2rem]">
-            {firstPokemon.data?.name}
-          </div>
-        </div>
-        <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col">
-          <img
-            src={secondPokemon.data?.sprites.front_default!}
-            className="w-full"
-          />
-          <div className="text-xl text-center capitalize mt-[-2rem]">
-            {secondPokemon.data?.name}
-          </div>
-          <div className="p-2"></div>
-        </div>
+        {!firstPokemon.isLoading &&
+          firstPokemon.data &&
+          !secondPokemon.isLoading &&
+          secondPokemon.data && (
+            <>
+              <PokemonListing
+                pokemon={firstPokemon.data}
+                vote={() => voteForRoundest(first)}
+              />
+              <div className="p-8">Vs</div>
+              <PokemonListing
+                pokemon={secondPokemon.data}
+                vote={() => voteForRoundest(second)}
+              />
+            </>
+          )}
+        <div className="p-2"></div>
       </div>
     </div>
   );
 };
 
-export default Home;
+type PokemonFromServer = inferQueryResponse<"get-pokemon-by-id">;
+
+const PokemonListing: FC<{ pokemon: PokemonFromServer; vote: () => void }> = (
+  props
+) => {
+  return (
+    <div className="flex flex-col items-center">
+      <img src={props.pokemon.sprites.front_default!} className="w-64 h-64" />
+      <div className="text-xl text-center capitalize mt-[-2rem]">
+        {props.pokemon.name}
+      </div>
+      <button className={btn} onClick={() => props.vote()}>
+        Rounder
+      </button>
+    </div>
+  );
+};
