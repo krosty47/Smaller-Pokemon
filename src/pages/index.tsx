b708bfd2e30027a9ebac351f99f4518fa15f07c5
krosty47 <c_coriasco@hotmail.com>
2022-01-19 10:08:38 -0300

    random Pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,18 @@
 import type { NextPage } from "next";
 import { trpc } from "@/utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Krosty" }]);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (data) return <div>{data.greeting}</div>;
+  const [first, second] = getOptionsForVote();
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-center items-center">
       <div className="text-2x1 text-center">Which Pokemón is Smaller?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex items-center justify-between max-w-2x1">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
